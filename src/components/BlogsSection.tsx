87fd70ea581e6e0d611f745a87c29c4b29f298b4
RepoForpract <159107766+RepoForpract@users.noreply.github.com>
2025-05-17 01:01:59 +0530

    Update BlogsSection.tsx

--- src/components/BlogsSection.tsx
+++ src/components/BlogsSection.tsx
@@ -12,16 +12,16 @@ const blogPosts = [
     slug: "what-is-kyverno-policy-management-using-kyverno"
   },
   {
+    title: "Getting Started with Microsoft Semantic Kernel: A Simple Guide to Prompts and Plugins in Python",
+    brief: "Semantic Kernel is an open-source SDK by Microsoft that lets you combine code (your logic and plugins) with language models (like OpenAI's GPT or Azure OpenAI) to build AI-first applications..",
+    dateAdded: "May 2024",
+    slug: "Getting-started-with-microsoft-semantic-kernel-a-simple-guide-to-prompts-and-plugins-in-python"
+  },
+  {
     title: "Mogenius: Making Kubernetes Feel Effortless for Developers",
     brief: "Discover how Mogenius simplifies Kubernetes for developers, making cloud-native application deployment and management accessible without deep infrastructure expertise.",
     dateAdded: "May 2024",
     slug: "mogenius-making-kubernetes-feel-effortless-for-developers"
-  },
-  {
-    title: "Mastering OpenShift: The Architecture Behind Enterprise-Grade Kubernetes",
-    brief: "Dive into OpenShift's architecture and discover how it extends Kubernetes to provide an enterprise-ready container platform with enhanced security and developer tools.",
-    dateAdded: "April 2024",
-    slug: "mastering-openshift-the-architecture-behind-enterprise-grade-kubernetes"
   },
   {
     title: "Useful Terraform Commands You Need to Know",
@@ -44,20 +44,21 @@ const BlogCard = ({ blog, index }: { blog: any, index: number }) => (
       <h3 className="text-xl font-bold text-portfolioLightestSlate mb-2">{blog.title}</h3>
       <p className="text-portfolioSlate mb-4 flex-grow">{blog.brief}</p>
       <Button
-        asChild
-        variant="outline"
-        className="w-fit text-portfolioTeal border-portfolioTeal/50 hover:bg-portfolioTeal/10 hover:text-portfolioTeal hover:border-portfolioTeal"
-      >
-        <a
-          href={`https://harshalr.hashnode.dev/${blog.slug}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center"
-        >
-          Read More
-          <ArrowRight className="ml-2 h-4 w-4" />
-        </a>
-      </Button>
+  asChild
+  variant="outline"
+  className="w-fit text-black border-black hover:bg-black/10 hover:text-black hover:border-black"
+>
+  <a
+    href={`https://harshalr.hashnode.dev/${blog.slug}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="flex items-center"
+  >
+    Read More
+    <ArrowRight className="ml-2 h-4 w-4" />
+  </a>
+</Button>
+
     </div>
   </div>
 );
